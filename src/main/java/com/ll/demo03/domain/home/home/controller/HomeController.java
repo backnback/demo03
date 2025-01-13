aa14dbebb6a296bd6056ab1a5cffa95208533e14
backnback <garam8796@naver.com>
2025-01-13 20:41:21 +0900

    test

--- src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/demo03/domain/home/home/controller/HomeController.java
@@ -15,7 +15,7 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Hello, World!!!!!, on ㅗㅗ " + siteName;
+        return "Hello, World!!!!!, on !ㅗㅗ! " + siteName;
     }
 
     @GetMapping("/secretKey")
